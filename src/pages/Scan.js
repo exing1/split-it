27eb20ab99a73104a61e3e8f803ecbcafcb820dd
exing1 <edward_xing@brown.edu>
2023-01-28 15:03:28 -0500

    proto scan page

--- src/pages/Scan.js
+++ src/pages/Scan.js
@@ -1,5 +1,28 @@
+import TopBar from "../components/TopBar";
+import { Link } from "react-router-dom";
+
 const Scan = () => {
-    return <h1>Scan</h1>;
+    const get_image = (img) => {
+        console.log(img)
+    }
+    return (
+        <>   
+            <TopBar prev=""/>
+            <div className="box">
+                <div className="upload">
+                    <form className="upload-form">
+                        <input type="file" id="file-upload"/>
+                        <Link to="/tip">
+                            <input type="submit"/>
+                        </Link>
+                    </form>
+                </div>
+                <div className="input">
+                    input manually
+                </div>
+            </div>
+        </>
+    );
 };
   
 export default Scan;
